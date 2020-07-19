b99d92d07e3872b82c3c263bb320373b33c94ef5
Masashi <examamu@gmail.com>
2020-07-20 00:03:53 +0900

    nullでのエラーの対応

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -47,7 +47,9 @@
         <!--時間の表示-->
     @foreach( $times as $time )
                         <input type = "hidden" name = "time{{ $i }}{{ $loop->iteration-1 }}" value = "{{ $time }}">
+        @if(isset($get_weekly_schedules[$weekly_array[$i]][$time]['schedule_id']) === TRUE)
                         <input type = "hidden" name = "schedule_id{{ $i }}{{ $loop->iteration-1 }}" value ="{{ $get_weekly_schedules[$weekly_array[$i]][$time]['schedule_id'] }}">
+        @endif
                         <tr>
                             <th class = "table_time" scope="row">{{ $time }}</th>
                             <td class = "table_customer_name">
